351b8689d901ead7e161f4b14fe61ed2fd61326b
Changgyu39 <94962815+Changgyu39@users.noreply.github.com>
2025-04-03 12:46:43 +0900

    Create homework3-2.php

---
+++ homework3-2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head><meta charset="utf-8"></head>
+
+<?php
+$n = 30;
+for($i = 0; $i<$n; $i++){
+    $data[$i] = rand(10,100);
+    echo("$data[$i] ");
+}
+sort($data);
+echo("<br> sorting ... <br>");
+for($i = 0; $i<$n; $i++){
+    echo("$data[$i] ");
+}
+
+?>
