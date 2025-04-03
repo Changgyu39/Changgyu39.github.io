e2ab498e8b46c527453db2710697705e16576764
Changgyu39 <94962815+Changgyu39@users.noreply.github.com>
2025-04-03 12:50:07 +0900

    Create (test)homework3-4.php

---
+++ (test)homework3-4.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head><meta charset="utf-8"></head>
+<?php
+echo("width <b>". $_GET["width"]. "</b> 이고.<p>");
+echo("height <b>". $_GET["height"]. "</b> 이다.<p>");
+$area = $_GET["width"]*$_GET["height"]/2.
+echo("삼각형 면적은 <b>". %area "</b> 이다.<p>");
+?>
